3fc7ba443acf35745884de35466914870dd4626a
Szymon Sasin <szymon@be-functional.uk>
2022-05-08 19:07:12 +0100

    improved team object

--- src/Functional.SplinterBots.BattleAPI/Team.fs
+++ src/Functional.SplinterBots.BattleAPI/Team.fs
@@ -3,17 +3,11 @@
 open Functional.SplinterBots.API
 
    
-type Team (summoner: Cards.Card, team: Cards.Card list) =
-    let teamHash = ""
-    let secret = ""
-
-    member this.GetHash () = 
-        teamHash
-    member this.GetSecret () = 
-        secret
-    member this.GetSummoner () =
-        summoner.card_detail_id
-    member this.GetTeam () = 
-        team 
-        |> Seq.map (fun m -> m.card_detail_id)
-        |> Array.ofSeq
+type Team (summoner: Cards.Card, team: Cards.Card seq) =
+    let secret = API.generateRandomString 10
+    member this.Summoner 
+        with get () = summoner
+    member this.Team 
+        with get () = team
+    member this.Secret 
+        with get () = secret
