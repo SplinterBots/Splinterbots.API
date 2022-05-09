4e1b083b8e8c071007c1203db3705ef4fda07e59
Szymon Sasin <szymon@be-functional.uk>
2022-05-09 17:58:12 +0100

    fight now takes a getteam only

--- src/Functional.SplinterBots.BattleAPI/Battler.fs
+++ src/Functional.SplinterBots.BattleAPI/Battler.fs
@@ -12,49 +12,21 @@ module Battler =
     type Username = string
     type PostingKey = string
         
-    type StartFight = unit -> Async<Transaction>
     type GetTeam = MatchDetails -> Async<Team>
-    type SubmitTeam = Transaction -> Team -> Async<Transaction>
-    type RevealTeam = Transaction -> Team -> Async<unit> 
-
-    let fight
-            (startFight: StartFight) 
-            (getTeam: GetTeam) 
-            (submitTeam: SubmitTeam) 
-            (revealTeam: RevealTeam)
-            (webSocket: WebSocketListener) =
+    
+    let private startNewMatch username postingkey =
         async {
-            let! transaction = startFight ()
-            do! webSocket.WaitForTransaction transaction.id
-            do! webSocket.WaitForGamesState GameState.match_found 
-
-            let matchDetails = webSocket.GetState GameState.match_found |> MatchDetails.bind
-            let! team = getTeam matchDetails
-
-            do! (Task.Delay 5000 |> Async.AwaitTask)
-
-            let! submitedTeam = submitTeam transaction team
-            let _ = webSocket.WaitForTransaction submitedTeam.id
-
-            do! revealTeam transaction team
-
-            do! webSocket.WaitForGamesState GameState.opponent_submit_team
+            let customJson = 
+                sprintf "{\"match_type\":\"Ranked\",\"app\":\"%s\",\"n\":\"%s\"}"
+                |> API.createCustomJsonPostingKey username "sm_find_match"
+            let! response = 
+                API.hive.create_transaction ([| customJson |], [| postingkey |])
+                |> Generator.getStringForSplinterlandsAPI
+                |> API.executeApiPostCall<Transaction> API.battleUrl 
+            return response
         }
-
-    let startNewMatch username postingkey =
-        fun () -> 
-            async {
-                let customJson = 
-                    sprintf "{\"match_type\":\"Ranked\",\"app\":\"%s\",\"n\":\"%s\"}"
-                    |> API.createCustomJsonPostingKey username "sm_find_match"
-                let! response = 
-                    API.hive.create_transaction ([| customJson |], [| postingkey |])
-                    |> Generator.getStringForSplinterlandsAPI
-                    |> API.executeApiPostCall<Transaction> API.battleUrl 
-                return response
-            }
-          
-    let submitTeam username postingKey (transaction: Transaction) (team: Team) = 
+              
+    let private submitTeam username postingKey (transaction: Transaction) (team: Team) = 
         async {
             let custom_Json = 
                 sprintf "{\"trx_id\":\"%s\",\"team_hash\":\"%s\",\"app\":\"%s\",\"n\":\"%s\"}"
@@ -67,8 +39,8 @@ module Battler =
                 |> API.executeApiPostCall<Transaction> API.battleUrl
             return response
         }
-
-    let revealTeam username postingKey (transaction: Transaction) (team: Team) = 
+    
+    let private revealTeam username postingKey (transaction: Transaction) (team: Team) = 
         async {
             let monsters = 
                 team.Team
@@ -81,11 +53,34 @@ module Battler =
                     team.Summoner.card_long_id
                     monsters
                     team.Secret
-
+    
             let custom_Json = API.createCustomJsonPostingKey username "sm_team_reveal" json
             let postData = 
                 API.hive.create_transaction([| custom_Json |], [| postingKey |])
                 |> Generator.getStringForSplinterlandsAPI 
-               
+                   
             do! API.executeApiPostCall<Transaction> API.battleUrl postData |> Async.Ignore
+        }
+
+    let fight
+            username 
+            postingKey
+            (webSocket: WebSocketListener)
+            (getTeam: GetTeam) =
+        async {
+            let! transaction = startNewMatch username postingKey
+            do! webSocket.WaitForTransaction transaction.id
+            do! webSocket.WaitForGamesState GameState.match_found 
+
+            let matchDetails = webSocket.GetState GameState.match_found |> MatchDetails.bind
+            let! team = getTeam matchDetails
+
+            do! (Task.Delay 5000 |> Async.AwaitTask)
+
+            let! submitedTeam = submitTeam username postingKey transaction team
+            let _ = webSocket.WaitForTransaction submitedTeam.id
+
+            do! revealTeam username postingKey transaction team
+
+            do! webSocket.WaitForGamesState GameState.opponent_submit_team
         }
