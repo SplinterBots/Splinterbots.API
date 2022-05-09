b25c756344c88c4a505fdbbe2b05d211d2424289
Szymon Sasin <szymon@be-functional.uk>
2022-05-09 13:38:55 +0100

    added team hashin capabilities

--- src/Functional.SplinterBots.BattleAPI/Team.fs
+++ src/Functional.SplinterBots.BattleAPI/Team.fs
@@ -1,13 +1,25 @@
 ﻿namespace Functional.SplinterBots.BattleAPI
 
 open Functional.SplinterBots.API
-
-   
+  
 type Team (summoner: Cards.Card, team: Cards.Card seq) =
     let secret = API.generateRandomString 10
+    let teamHash =
+        let monstersString = 
+            team
+            |> Seq.map (fun monster -> monster.card_long_id)
+            |> String.concat ","
+        sprintf "%s,%s,%s"
+            summoner.card_long_id
+            monstersString
+            secret
+        |> generateMD5Hash
+
     member this.Summoner 
         with get () = summoner
     member this.Team 
         with get () = team
     member this.Secret 
         with get () = secret
+    member this.TeamHash
+        with get () = teamHash
